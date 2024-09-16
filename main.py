f58cfd82cf94ff2e5a2a62e9767a591527362488
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2024-09-16 14:48:32 +0200

    UPD page statics & templates

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from typing import Union
-from fastapi import FastAPI
+
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse, HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 from dotenv import load_dotenv
 import os
 
 load_dotenv()  # take environment variables from .env.
 
-print('TEST:', os.environ['TEST'])
-print('BDD_URL:', os.environ['BDD_URL'])
+#print('TEST:', os.environ['TEST'])
+#print('BDD_URL:', os.environ['BDD_URL'])
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return RedirectResponse("/static/index.html") # {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def read_item(request: Request, item_id: int, q: Union[str, None] = None):
+    #return {"item_id": item_id, "q": q}
+    return templates.TemplateResponse(
+        request=request, name="item.html", context={"item_id": item_id, "q": q}
+    )
 
