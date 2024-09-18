966ff6bd1b2b7f4c0f17bba303e628398963f4f1
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2024-09-18 14:27:03 +0200

    UPD demo SQLAlchemy ajout colonne surname & liste des User dans BDD

--- demo_sqlalchemy.py
+++ demo_sqlalchemy.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, select
 from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 import os, random
@@ -13,6 +13,10 @@ class User(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String(50))
+    surname = Column(String(50))
+
+    def __repr__(self) -> str:
+        return f"User[{self.id}] : {self.name}"
 
 # Connexion à la base de données
 SQLALCHEMY_DATABASE_URL=os.getenv('SQLALCHEMY_DATABASE_URL')
@@ -28,6 +32,15 @@ session = Session()
 
 # Insertion d'un nouvel utilisateur
 n=random.randint(0, 999999)
-new_user = User(id=n, name=f"John Doe-{n}")
+new_user = User(id=n, name=f"John Doe-{n}", surname="rien")
+print('USER:', new_user)
 session.add(new_user)
+
+
+stmt = select(User)
+print(stmt)
+print('----------------------------')
+for user in session.scalars(stmt):
+    print(user)
+
 session.commit()
